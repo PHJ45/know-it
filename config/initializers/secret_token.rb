fb517d68b8962c8ebd1a153e90193e2edd9ef921
Patrick Janson <pjlacrosse@gmail.com>
2013-12-16 19:04:52 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Knowitowl::Application.config.secret_key_base = 'e42d51f256aa983e992c7e1416638eab7d9535a430bdc2eddcdf47cfd19cd72cd274e623721bd5004e5ff2f309247d8b07b8bfebcfa28f338ae954c1d95c51ed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Knowitowl::Application.config.session_store :cookie_store, key: '_knowitowl_session'
