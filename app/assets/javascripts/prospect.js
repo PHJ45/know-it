b4b73cffb42317ec4b15dc76eccd9bc982161a36
Patrick Janson <pjlacrosse@gmail.com>
2013-12-20 12:51:51 -0500

    adding js work

--- app/assets/javascripts/prospect.js
+++ app/assets/javascripts/prospect.js
@@ -1,4 +1,9 @@
-$(document).ready(function() {
+$( document ).ready(function() {
 
+    $("#recomendation-text").click(function(){
+      $(".call-to").fadeOut(500, function(){
+        $(".call-to").fadeIn(1000);
+      });
+    }); 
 
 });
