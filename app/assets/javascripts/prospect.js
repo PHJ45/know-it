ecd31effea7135dd92ca683031a7c7399962a7db
Patrick Janson <pjlacrosse@gmail.com>
2013-12-24 16:21:50 -0500

    adding fades to inputs

--- app/assets/javascripts/prospect.js
+++ app/assets/javascripts/prospect.js
@@ -1,14 +1,18 @@
 $( document ).ready(function() {
 
-  $('div.fade-form').fadeTo('slow',0.35);
+  //Form Fade
 
+  $('div.fade-form-first').fadeTo(1000,0.4);
+  
   $('div.fade-form').mouseenter(function(){
     $(this).fadeTo('fast',1);
   });
 
   $('div.fade-form').mouseleave(function(){
-    $(this).fadeTo('fast',0.5);
+    $(this).fadeTo('fast',0.4);
   });
+
+  //Menu Open
 
   $('.glyphicon-question-sign').click(function(){
     $('.wrapper').css('background-color','rgb(0,158,157)');
