bf6fd8ddbb15db3be5af28b7801ea0c5d73ebbce
Patrick Janson <pjlacrosse@gmail.com>
2013-12-24 16:05:13 -0500

    trying diff fade times

--- app/assets/javascripts/prospect.js
+++ app/assets/javascripts/prospect.js
@@ -1,7 +1,17 @@
 $( document ).ready(function() {
 
+  $('div.fade-form').fadeTo('slow',0.35);
+
+  $('div.fade-form').mouseenter(function(){
+    $(this).fadeTo('fast',1);
+  });
+
+  $('div.fade-form').mouseleave(function(){
+    $(this).fadeTo('fast',0.5);
+  });
+
   $('.glyphicon-question-sign').click(function(){
     $('.wrapper').css('background-color','rgb(0,158,157)');
   });
-
+    
 });
